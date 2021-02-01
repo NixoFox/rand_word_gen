d4dee1a296672f070648b4f81dd3fce55ace45a6
NixoFox <78245568+NixoFox@users.noreply.github.com>
2021-02-01 17:12:14 +0330

    init

---
+++ rwg.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+import random
+
+times = int(input("how many words? "))
+isadj = bool(int(input("should have adjectives? ")))
+
+nouns = "/home/nixo/Public/RWG/nouns.txt"
+adjectives = "/home/nixo/Public/RWG/adjectives.txt"
+
+def get_rand_line ( txt ):
+    with open(txt) as f:
+        lines = f.readlines()
+        var = lines[random.randint(0, len(lines))]
+        var = var[:-1]
+    return var
+
+print("")
+for i in range(0, times):
+    word = ""
+    word = get_rand_line(nouns)
+    if isadj == True:
+        adj = get_rand_line(adjectives)
+        word = adj + " " + word
+    print ( word )
+print("")
+
