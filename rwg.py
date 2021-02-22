c6f62bf35acd70e640f417c08eec0f5dee98ca49
NixoFox <78245568+NixoFox@users.noreply.github.com>
2021-02-22 11:28:42 +0330

    Catch user input errors, import randint only

--- rwg.py
+++ rwg.py
@@ -1,26 +1,42 @@
 #!/bin/python3
-import random
+from random import randint
 
-times = int(input("how many words? "))
-isadj = bool(int(input("should have adjectives? ")))
-
+times = 0
+is_adj = False
 nouns = "nouns.txt"
 adjectives = "adjectives.txt"
+
+def get_amount():
+  global times
+  try:
+    times = int(input("amount: "))
+  except:
+    print("error: invalid value")
+    get_amount()
+get_amount()
+
+def get_is_adj():
+  global is_adj
+  try:
+    is_adj = bool(int(input("adjectives [0, 1]: ")))
+  except:
+    print("error: invalid value")
+    get_is_adj()
+get_is_adj()
 
 def get_rand_line ( txt ):
     with open(txt) as f:
         lines = f.readlines()
-        var = lines[random.randint(0, len(lines))]
+        var = lines[randint(0, len(lines))]
         var = var[:-1]
     return var
 
-print()
+print("")
 for i in range(0, times):
     word = ""
     word = get_rand_line(nouns)
-    if isadj == True:
+    if is_adj == True:
         adj = get_rand_line(adjectives)
         word = adj + " " + word
     print ( word )
-print()
-
+print("")
